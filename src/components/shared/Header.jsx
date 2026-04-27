75dbbbd968f6860a76eb3243f37314616326a48c
Hridrita <moonsaha721@gmail.com>
2026-04-27 16:34:53 +0600

    added font and dare fns

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React from "react";
+import logo from "../../assets/logo.png";
+import Image from "next/image";
+import { format } from "date-fns";
 
 const Header = () => {
-    return (
-        <div>
-            <h2>Hedaer</h2>
-        </div>
-    );
+  return (
+    <div className="mx-auto">
+      <div className="text-center py-8 space-y-2">
+        <Image src={logo} alt="logo" width={300} height={200}></Image>
+        <p>Journalism Without Fear or Favour</p>
+        <p>{format(new Date(), "EEEE, MMM dd, yyyy")}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
